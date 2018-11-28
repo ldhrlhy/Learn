1045d50bbdd564da8730db4046784813db946ffc
dy <dy@dydeMacBook-Pro.lan>
2018-11-28 09:36:27 +0800

    堆

--- src/sort/BubbleSort.java
+++ src/sort/BubbleSort.java
@@ -8,6 +8,10 @@ public class BubbleSort {
             System.out.print(values[i]);
 
         }
+        String str1 = new StringBuilder("计算机").append("软件").toString();
+        System.out.println(str1.intern()==str1);
+        String str2 = new StringBuilder("a").append("bc").toString();
+        System.out.println(str2.intern()==str2);
 
     }
     public static void bubbleSort(int a[],int n){
